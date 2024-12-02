097c3a4893478bc368de0158ac617af80f0f8016
mark <m.derooij1@rocva.nl>
2024-12-02 11:03:55 +0100

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+/*Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');*/
+
+Route::get('articles', [ArticleController::class, 'index']);
+Route::get('articles/{article}', [ArticleController::class, 'show']);
+Route::post('articles', [ArticleController::class, 'store']);
+Route::put('articles/{article}', [ArticleController::class, 'update']);
+Route::delete('articles/{article}', [ArticleController::class, 'delete']);
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/hello', function () {
+    return view('welcome');
+});
+
+Route::get('/users', [UserController::class, 'index']);
+
+Route::get('/', function () {
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+    return view('welcome', ['planeten' => $planets]);
+});
+
+Route::get('/test/{id}', function (string $id) {
+    return view('test', ['id' => $id]);
+});
