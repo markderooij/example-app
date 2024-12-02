cf41594403ddc3d482502e32771943773214f95b
mark <m.derooij1@rocva.nl>
2024-12-02 14:37:24 +0100

    planets route

---
+++ resources/views/planets.blade.php
@@ -0,0 +1,6 @@
+<div>
+    @foreach ($planeten as $planeet)
+        {{ $planeet['name']}}
+    @endforeach
+    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
+</div>
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,31 @@ Route::get('/hello', function () {
 
 Route::get('/users', [UserController::class, 'index']);
 
-Route::get('/', function () {
+Route::get('planets', function () {
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+
+    return view('planets', ['planeten' => $planets]);
+    
+});
+
+/*Route::get('/', function () {
     $planets = [
         [
             'name' => 'Mars',
@@ -29,7 +53,7 @@ Route::get('/', function () {
         ],
     ];
     return view('welcome', ['planeten' => $planets]);
-});
+});*/
 
 Route::get('/test/{id}', function (string $id) {
     return view('test', ['id' => $id]);
